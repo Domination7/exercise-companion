0b59f545e15e6824b62d06920797cfe6499989f2
gen3344 <31801630+gen3344@users.noreply.github.com>
2018-03-06 22:18:28 -0600

    added many features (color scales)

--- app/src/main/java/com/example/exercisecompanion/DisplayMessageActivity.java
+++ app/src/main/java/com/example/exercisecompanion/DisplayMessageActivity.java
@@ -1,8 +1,10 @@
 package com.example.exercisecompanion;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -19,26 +21,61 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra("EXTRA_MESSAGE");
-        String numStars = intent.getStringExtra("NUM_STARS");
+        //String numStars = intent.getStringExtra("NUM_STARS");
         String weightStr = intent.getStringExtra("WEIGHT_VALUE");
-        String heightStr = intent.getStringExtra("HEIGHT_VALUE");
+        String heightFtStr = intent.getStringExtra("HEIGHT_VALUE_FT");
+        String heightInStr = intent.getStringExtra("HEIGHT_VALUE_IN");
 
         TextView textView = findViewById(R.id.textView);
-        TextView textView4 = findViewById(R.id.textView4);
+        //TextView textView4 = findViewById(R.id.textView4);
         TextView textView2 = findViewById(R.id.textView2);
 
         textView.setText("Hi there " + message);
-        textView4.setText(numStars);
-        textView2.setText("Your BMI is " + calcBmi(weightStr,heightStr));
+        //textView4.setText(numStars);
+        textView2.setText("Your BMI is " + calcBmi(weightStr,heightFtStr,heightInStr));
     }
 
-    public double calcBmi(String weight, String height){
+    public double calcBmi(String weight, String heightFt, String heightIn){
         DecimalFormat oneD = new DecimalFormat("##.##");
         double weightVal = Double.parseDouble(weight);
-        double heightVal = Double.parseDouble(height);
+        double heightVal = Double.parseDouble(heightFt) + (Double.parseDouble(heightIn)/12);
+
         weightVal *= .453592;
         heightVal *= .3048;
         double bmi = Double.valueOf(oneD.format((weightVal/heightVal)/heightVal));
+
+        View underweight = findViewById(R.id.underweight);
+        View normal = findViewById(R.id.normal);
+        View overweight = findViewById(R.id.overweight);
+        View obese = findViewById(R.id.obese);
+        View exObese = findViewById(R.id.extremeObese);
+        TextView textView3 = findViewById(R.id.textView3);
+
+        if(bmi < 18.5){
+            underweight.setBackgroundColor(getResources().getColor(R.color.underweight));
+            textView3.setText("Underweight BMI");
+            textView3.setTextColor(getResources().getColor(R.color.underweight));
+        }
+        else if(bmi >= 18.5 && bmi < 25) {
+            normal.setBackgroundColor(getResources().getColor(R.color.normal));
+            textView3.setText("Healthy BMI");
+            textView3.setTextColor(getResources().getColor(R.color.normal));
+        }
+        else if(bmi >= 25 && bmi < 30) {
+            overweight.setBackgroundColor(getResources().getColor(R.color.overweight));
+            textView3.setText("Overweight BMI");
+            textView3.setTextColor(getResources().getColor(R.color.overweight));
+        }
+        else if(bmi >= 30 && bmi < 40) {
+            obese.setBackgroundColor(getResources().getColor(R.color.obese));
+            textView3.setText("Obesity BMI");
+            textView3.setTextColor(getResources().getColor(R.color.obese));
+        }
+        else if(bmi >= 40) {
+            exObese.setBackgroundColor(getResources().getColor(R.color.extremeObese));
+            textView3.setText("Extreme Obesity BMI");
+            textView3.setTextColor(getResources().getColor(R.color.extremeObese));
+        }
         return bmi;
     }
 }
--- app/src/main/java/com/example/exercisecompanion/MainActivity.java
+++ app/src/main/java/com/example/exercisecompanion/MainActivity.java
@@ -21,19 +21,22 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
 
         EditText editText = findViewById(R.id.editText);
-        RatingBar ratingBar = findViewById(R.id.ratingBar2);
+        //RatingBar ratingBar = findViewById(R.id.ratingBar2);
         EditText editText2 = findViewById(R.id.editText2);
         EditText editText3 = findViewById(R.id.editText3);
+        EditText editText4 = findViewById(R.id.editText4);
 
         String message =  editText.getText().toString();
-        String numStars = String.valueOf(ratingBar.getRating());
+        //String numStars = String.valueOf(ratingBar.getRating());
         String weightStr = editText2.getText().toString();
-        String heightStr = editText3.getText().toString();
+        String heightFtStr = editText3.getText().toString();
+        String heightInStr = editText4.getText().toString();
 
-        intent.putExtra("NUM_STARS",numStars);
+        //intent.putExtra("NUM_STARS",numStars);
         intent.putExtra("EXTRA_MESSAGE",message);
         intent.putExtra("WEIGHT_VALUE",weightStr);
-        intent.putExtra("HEIGHT_VALUE",heightStr);
+        intent.putExtra("HEIGHT_VALUE_FT",heightFtStr);
+        intent.putExtra("HEIGHT_VALUE_IN",heightInStr);
 
         startActivity(intent);
     }
